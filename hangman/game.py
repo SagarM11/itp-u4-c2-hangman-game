d0a56d27bf4745874eb95fc74f148cd70d9aef21
SagarM11 <sagarm11@gmail.com>
2020-04-19 19:17:38 +0530

    Update game.py

--- hangman/game.py
+++ hangman/game.py
@@ -11,7 +11,7 @@ def _get_random_word(list_of_words):
 
 def _mask_word(word):
     if len(word) == 0:
-        raise InvalidWordException())
+        raise InvalidWordException()
     return('*'* len(word))
 
 def _uncover_word(answer_word, masked_word, character):
