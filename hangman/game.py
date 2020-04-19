12f9d32d3651f6988bcb6b6939763e3ebc54d1da
SagarM11 <sagarm11@gmail.com>
2020-04-19 16:58:05 +0530

    Update game.py

--- hangman/game.py
+++ hangman/game.py
@@ -1,24 +1,49 @@
 from .exceptions import *
+import random
 
 # Complete with your own, just for fun :)
-LIST_OF_WORDS = []
-
+LIST_OF_WORDS = ['krishna','madhusudan','nrsimha']
 
 def _get_random_word(list_of_words):
-    pass
-
+    return(list_of_words[randint(0, len(list_of_words))])
 
 def _mask_word(word):
-    pass
-
+    if len(word) == 0:
+        raise(InvalidWordException)
+    return('*'* len(word))
 
 def _uncover_word(answer_word, masked_word, character):
-    pass
-
+    if len(answer_word)== 0 and len(masked_word) == 0:
+        raise(InvalidWordException)
+    elif len(masked_word) > 1:
+        raise(InvalidGuessedLetterException)
+    elif len(masked_word) > len(answer_word):
+        raise(InvalidWordException)
+    else:    
+        index = [i for i,x in enumerate(list(answer_word)) if x==uppercase(character) or x==lowercase(character)]
+        for a in index:
+            masked_word[index] = character
+    
+    return(masked_word)
 
 def guess_letter(game, letter):
-    pass
-
+    
+    if game['masked_word'] == answer_word:
+        raise(GameFinishedException)
+    else:
+        prev_word = game['masked_word']
+        game['masked_word'] = _uncover_word(game['answer_word'], game['masked_word'],letter)
+        game['previous_guess'] = game['previous_guess'].append(letter)
+        
+        if game['masked_word'] == prev_word:
+            game['remaining_misses'] = game['remaining_misses'] - 1
+        
+        if game['masked_word'] == answer_word:
+            raise(GameWonException)
+        elif remaining_misses <= 0:
+            raise(GameLostException)
+     
+    return(game)
 
 def start_new_game(list_of_words=None, number_of_guesses=5):
     if list_of_words is None:
@@ -33,4 +58,4 @@ def start_new_game(list_of_words=None, number_of_guesses=5):
         'remaining_misses': number_of_guesses,
     }
 
-    return game
+    return(game)
